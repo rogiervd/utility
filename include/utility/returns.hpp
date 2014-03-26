c38a36af667020c2bb7d786a4cfdc324225f0fc0
Rogier van Dalen <rogiervd@gmail.com>
2014-03-26 16:41:52 +0000

    Introduce returns.hpp, which makes it easier to use "auto" return types
    Ignore-this: 439209e9d37e280bcc0f4cf5e4512453
    
    darcs-hash:20140326164152-c9a90-db9bfb27d0cac85157b3b370110986e2d4ef284e

---
+++ include/utility/returns.hpp
@@ -0,0 +1,38 @@
+/*
+Copyright 2011, 2012 Rogier van Dalen.
+
+This file is part of Rogier van Dalen's Utility library for C++.
+
+This library is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/** \file
+Define macro to make it easier to declare the return type of a simple function.
+*/
+
+#ifndef UTILITY_RETURNS_HPP_INCLUDED
+#define UTILITY_RETURNS_HPP_INCLUDED
+
+/**
+In C++11, an "auto" return type needs an explicit type specification after the
+traditional function signature.
+This looks like
+    auto f (...) -> decltype (x) { return x; }
+This macro simplifies those expressions into
+    auto f (...) RETURNS (x)
+*/
+#define RETURNS(...) -> decltype(__VA_ARGS__) { return __VA_ARGS__; }
+
+#endif // UTILITY_RETURNS_HPP_INCLUDED
+
---
+++ test/utility/test-returns.cpp
@@ -0,0 +1,53 @@
+/*
+Copyright 2013 Rogier van Dalen.
+
+This file is part of Rogier van Dalen's Utility library for C++.
+
+This library is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#define BOOST_TEST_MODULE test_utility_returns
+#include "utility/test/boost_unit_test.hpp"
+
+#include "utility/returns.hpp"
+
+#include <type_traits>
+
+#include <boost/mpl/assert.hpp>
+
+// Standard case.
+template <class Type> inline auto return_decayed (Type c)
+RETURNS (c)
+
+// More complicated case that seems to expose a bug on GCC 4.6 when the
+// argument to return is in parentheses.
+template <class Type> struct construct_default_helper {
+    Type operator() () const { return Type(); }
+};
+
+template <class Type> inline auto construct_default()
+RETURNS (construct_default_helper <Type>()())
+
+
+BOOST_AUTO_TEST_SUITE(test_utility_returns)
+
+BOOST_AUTO_TEST_CASE (test_utility_returns) {
+    BOOST_MPL_ASSERT ((std::is_same <decltype (return_decayed (1)), int>));
+
+    BOOST_MPL_ASSERT ((std::is_same <
+        decltype (construct_default <float>()), float>));
+    BOOST_CHECK_EQUAL (construct_default <float>(), 0);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
