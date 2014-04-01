cf9627da031bdc2364e0bf3acc06707baacae206
Rogier van Dalen <rogiervd@gmail.com>
2014-04-01 14:17:08 +0100

    Introduce shared.hpp
    Ignore-this: 5a6506dbc54645c909837229838e9ac3
    The lightweight "shared" class contains an atomic reference count, so that it have multiple owners.
    
    darcs-hash:20140401131708-c9a90-0a903166d2a714ec357c0642c58b66f0868247e3

---
+++ include/utility/shared.hpp
@@ -0,0 +1,165 @@
+/*
+Copyright 2006, 2007, 2009, 2014 Rogier van Dalen.
+
+This file is part of Rogier van Dalen's Utility library for C++.
+
+This library is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef UTILITY_SHARED_HPP_INCLUDED
+#define UTILITY_SHARED_HPP_INCLUDED
+
+#include <new>
+#include <type_traits>
+
+#include <boost/detail/atomic_count.hpp>
+#include <boost/utility/enable_if.hpp>
+
+#include "with_allocator.hpp"
+
+namespace utility {
+
+    class shared;
+
+    /**
+    Base class for reference-counted objects.
+    \sa small_ptr
+    */
+    class shared {
+    private:
+        boost::detail::atomic_count count;
+
+    protected:
+        shared() noexcept : count (0l) {}
+
+        // The copy and move constructors mustn't copy the count.
+        shared (shared const &) noexcept : count (0l) {}
+        shared (shared &&) noexcept : count (0l) {}
+
+        // Assignment mustn't copy the count.
+        shared & operator = (shared const &) noexcept { return *this; }
+        shared & operator = (shared &&) noexcept { return *this; }
+
+    public:
+        /**
+        Register as an owner of the object.
+        Call release_count later to der
+        */
+        static void acquire (shared * s) noexcept { ++ s->count; }
+
+        /**
+        Release a reference to a shared object.
+        \return true iff this was the last reference, i.e. iff the object must
+        be deleted.
+        */
+        static bool release_count (shared * s) noexcept
+        { return ! -- s->count; }
+
+        /**
+        \return The current number of owners of this object.
+        */
+        static long get_count (shared const * s) noexcept { return s->count; }
+
+        /**
+        Use an allocator to construct an object of type "Type" and return a
+        pointer.
+        This provides strong exception-safety.
+        If construction fails with an exception, the memory will be deallocated.
+        */
+        template <class Type, class Allocator, class ... Arguments>
+            static Type *
+            construct (Allocator & allocator, Arguments && ... arguments)
+        {
+            static_assert (std::is_base_of <shared, Type>::value,
+                "Type must be derived from 'shared'.");
+            static_assert (std::is_same <typename Allocator::value_type,
+                typename std::decay <Type>::type>::value,
+                "The allocator value_type must match small_ptr's Type.");
+
+            // This works in two steps, allocating memory and constructing the
+            // object.
+            // That gives a risk of memory leaks.
+
+            // Step 1: allocate memory.
+            // This might fail with, say, std::bad_alloc, but then no memory
+            // is leaked.
+            Type * object = allocator.allocate (1);
+
+            // Step 2: construct the object.
+            // But first tell our guard to deallocate the object if something
+            // goes wrong.
+            static bool const is_safe = noexcept (
+                Type (std::forward <Arguments> (arguments) ...));
+            guard <Type, Allocator, is_safe> g (object, allocator);
+            new (object) Type (std::forward <Arguments> (arguments) ...);
+            // If we end up here, construction has succeeded, and the memory
+            // does not need to be deallocated.
+            g.dismiss();
+
+            return object;
+        }
+
+        /**
+        Release ownership of the object.
+        If the ownership count drops to zero, destruct the object and deallocate
+        the memory from the allocator.
+        Does not set \a object to zero.
+        */
+        template <class Type, class Allocator>
+        static void release (Allocator & allocator, Type * object) noexcept {
+            // This seems to always on GCC 4.6.
+            // static_assert (noexcept (object->~Type()),
+            //     "Type must have a no-throw destructor.");
+            if (release_count (object)) {
+                // object must be deleted
+                object->~Type();
+                allocator.deallocate (object, 1);
+            }
+        }
+
+    private:
+        /**
+        Guard that deallocates memory if an exception is thrown.
+        */
+        template <class Type, class Allocator, bool safe> class guard {
+            Type * object;
+            Allocator & allocator;
+            bool set;
+        public:
+            guard (Type * object, Allocator & allocator) noexcept
+            : object (object), allocator (allocator), set (true) {}
+
+            ~guard() noexcept {
+                if (set)
+                    allocator.deallocate (object, 1);
+            }
+
+            void dismiss() noexcept { set = false; }
+        };
+
+        // Version for non-throwing constructors.
+        template <class Type, class Allocator>
+            class guard <Type, Allocator, true>
+        {
+        public:
+            guard (Type *, Allocator &) noexcept {}
+            ~guard() noexcept {}
+            void dismiss() noexcept {}
+        };
+
+    };
+
+} // namespace utility
+
+#endif // UTILITY_SHARED_HPP_INCLUDED
---
+++ test/utility/test-shared.cpp
@@ -0,0 +1,116 @@
+/*
+Copyright 2014 Rogier van Dalen.
+
+This file is part of Rogier van Dalen's Utility library for C++.
+
+This library is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#define BOOST_TEST_MODULE test_utility_shared
+#include "utility/test/boost_unit_test.hpp"
+
+#include <memory>
+
+#include "utility/test/test_allocator.hpp"
+#include "utility/test/throwing.hpp"
+#include "utility/test/tracked.hpp"
+
+#include "utility/shared.hpp"
+
+BOOST_AUTO_TEST_SUITE(test_suite_utility_shared)
+
+struct shared : utility::shared {};
+
+BOOST_AUTO_TEST_CASE (test_utility_shared_basic) {
+    shared s;
+    BOOST_CHECK_EQUAL (shared::get_count (&s), 0);
+    shared::acquire (&s);
+    BOOST_CHECK_EQUAL (shared::get_count (&s), 1);
+    shared::acquire (&s);
+    BOOST_CHECK_EQUAL (shared::get_count (&s), 2);
+
+    BOOST_CHECK (!shared::release_count (&s));
+    BOOST_CHECK_EQUAL (shared::get_count (&s), 1);
+    BOOST_CHECK (shared::release_count (&s));
+    BOOST_CHECK_EQUAL (shared::get_count (&s), 0);
+}
+
+template <bool t1, bool t2, bool t3, bool t4, bool t5, bool t6>
+    struct test_exceptions
+{
+    struct shared_throwing : utility::shared {
+        utility::throwing <utility::tracked <int>, t1, t2, t3, t4, t5, t6>
+            content_;
+
+        shared_throwing (utility::thrower & thrower,
+            utility::tracked_registry & registry, int value)
+        : content_ (thrower, utility::tracked <int> (registry, value)) {}
+
+        int  & content()  { return content_.content().content(); }
+        int const & content() const { return content_.content().content(); }
+    };
+
+    void operator() (utility::thrower & thrower) const {
+        utility::tracked_registry registry;
+        {
+            // This must be scoped or the registry could go out of scope before
+            // this object.
+            shared_throwing on_stack (thrower, registry, 7);
+        }
+
+        utility::test_allocator <std::allocator <shared_throwing>> allocator (
+            thrower);
+
+        // Allocate and deallocate manually.
+        shared_throwing * on_heap = shared::construct <shared_throwing> (
+            allocator, thrower, registry, 234);
+        BOOST_CHECK_EQUAL (shared::get_count (on_heap), 0);
+        BOOST_CHECK_EQUAL (on_heap->content(), 234);
+        on_heap->~shared_throwing();
+        allocator.deallocate (on_heap, 1);
+
+        // Allocate manually, but then handle with acquire() and release().
+        on_heap = shared::construct <shared_throwing> (
+            allocator, thrower, registry, 789);
+        BOOST_CHECK_EQUAL (shared::get_count (on_heap), 0);
+        BOOST_CHECK_EQUAL (on_heap->content(), 789);
+        shared::acquire (on_heap);
+        BOOST_CHECK_EQUAL (shared::get_count (on_heap), 1);
+        shared::acquire (on_heap);
+        BOOST_CHECK_EQUAL (shared::get_count (on_heap), 2);
+        shared::release (allocator, on_heap);
+        BOOST_CHECK_EQUAL (shared::get_count (on_heap), 1);
+        shared::release (allocator, on_heap);
+        // The object should be destructed now.
+    }
+};
+
+template <int throw_points> void check_all_exceptions() {
+    utility::check_all_throw_points (test_exceptions <
+            throw_points & 0x01, throw_points & 0x02, throw_points & 0x04,
+            throw_points & 0x08, throw_points & 0x10, throw_points & 0x20>());
+    check_all_exceptions <throw_points - 1>();
+}
+
+template <> void check_all_exceptions <-1> () {}
+
+BOOST_AUTO_TEST_CASE (test_utility_shared_exceptions) {
+    // Check only a throwing allocator.
+    utility::check_all_throw_points (
+        test_exceptions <false, false, false, false, false, false>());
+    // Check all configurations.
+    check_all_exceptions <63>();
+}
+
+BOOST_AUTO_TEST_SUITE_END()
