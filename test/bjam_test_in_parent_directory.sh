c6dc6ed39574e283b8c2c483641f35a7a9269764
Rogier van Dalen <rogiervd@gmail.com>
2015-04-14 14:13:30 +0100

    Add configuration for continuous integration with Travis
    Travis CI is an online continuous integration service that automatically runs
    tests on commits and pull requests from GitHub.
    The .travis.yml file configures it.
    This one will run on gcc and clang (at Travis' default versions),
    in debug and release mode.
    
    Since this repository cannot be run by itself, the test script actually checks
    out "utility-test", a sort of shell around "utility", and runs the test from
    there.

---
+++ test/bjam_test_in_parent_directory.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Run this from the root directory of the "utility" repository.
+
+# This script will generate a directory ../utility-test and not remove it!
+# So watch out where you run it.
+
+# Run the tests by using this repository as submodule of the "utility-test"
+# repository.
+
+# This is necessary because this repository cannot be tested by itself.
+
+
+# Travis CI looks for this line.
+set -ev
+
+set -o nounset
+set -o errexit
+
+(
+    # Check out the "master" branch of "utility-test" from GitHub, in the parent
+    # directory.
+
+    cd ../
+    git clone git://github.com/rogiervd/utility-test.git
+    cd utility-test
+    git checkout master
+    git submodule init
+    git submodule update
+    # Then replace the "utility" submodule with the one in ../utility.
+    rm -r utility
+    ln -s ../utility
+
+    # Test it
+    bjam test "$@"
+
+)
